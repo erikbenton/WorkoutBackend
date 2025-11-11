531ed91ca9a4a2715f61705d6cea0bc1d005b279
Erik Benton <erik.benton@hotmail.com>
2025-11-11 12:31:06 -0800

    updating the ci_cd workflow to include azure deployment plus ability to populate the SQL database once deployed

--- WorkoutBackend.Api/Program.cs
+++ WorkoutBackend.Api/Program.cs
@@ -1,4 +1,5 @@
 using WorkoutBackend.Data.Repositories.CompletedWorkouts;
+using WorkoutBackend.Data.Repositories.Database;
 using WorkoutBackend.Data.Repositories.Exercises;
 using WorkoutBackend.Data.Repositories.Workouts;
 using WorkoutBackend.Data.Services;
@@ -24,6 +25,8 @@ builder.Services.AddScoped<IExerciseSetRepository, SqlExerciseSetRepository>(rep
 builder.Services.AddScoped<ICompletedWorkoutRepository, SqlCompletedWorkoutRepository>(repo => new SqlCompletedWorkoutRepository(workoutDbConnectionString));
 builder.Services.AddScoped<ICompletedExerciseGroupRepository, SqlCompletedExerciseGroupRepository>(repo => new SqlCompletedExerciseGroupRepository(workoutDbConnectionString));
 builder.Services.AddScoped<ICompletedExerciseSetRepository, SqlCompletedExerciseSetRepository>(repo => new SqlCompletedExerciseSetRepository(workoutDbConnectionString));
+
+builder.Services.AddScoped<IDatabaseRepository, SqlDatabaseRepository>(repo => new SqlDatabaseRepository(workoutDbConnectionString));
 
 // Workout services
 builder.Services.AddScoped<IWorkoutService, WorkoutService>();
---
+++ WorkoutBackend.Data/Repositories/Database/SqlDatabaseRepository.cs
@@ -0,0 +1,30 @@
+﻿
+using Dapper;
+using Microsoft.Data.SqlClient;
+using WorkoutBackend.Data.DataAccess;
+
+namespace WorkoutBackend.Data.Repositories.Database;
+
+public class SqlDatabaseRepository(string connectionString) : IDatabaseRepository
+{
+    private readonly string _connectionString = connectionString;
+
+    public async Task CreateAllTablesAsync()
+    {
+        using var connection = new SqlConnection(_connectionString);
+        await connection.ExecuteAsync(InitializationDataAccess.InitializeTables);
+    }
+
+    public async Task DropAllTablesAsync()
+    {
+        using var connection = new SqlConnection(_connectionString);
+        await connection.ExecuteAsync(InitializationDataAccess.DropAllTables);
+    }
+
+    public async Task PopulateDefaultValues()
+    {
+        using var connection = new SqlConnection(_connectionString);
+        await connection.ExecuteAsync(InitializationDataAccess.PopulateBodyParts);
+        await connection.ExecuteAsync(InitializationDataAccess.PopulateEquipment);
+    }
+}
