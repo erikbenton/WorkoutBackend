f036ec07cd5e1d71fea01162c2ec2f6f92f36a24
Erik Benton <erik.benton@hotmail.com>
2025-12-12 16:14:09 -0800

    updating Exercise so that no Name causes an error

--- WorkoutBackend.Core/Models/Exercise.cs
+++ WorkoutBackend.Core/Models/Exercise.cs
@@ -3,7 +3,7 @@
 public class Exercise
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
+    public string Name { get; set; }
     public string? Instructions { get; set; }
     public string? BodyPart { get; set; }
     public string? Equipment { get; set; }
