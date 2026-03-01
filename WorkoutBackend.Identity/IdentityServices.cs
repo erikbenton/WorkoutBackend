17ddf45fb5a477cc8b42aa33bd4c6463465e941b
Erik Benton <erik.benton@hotmail.com>
2026-03-01 13:13:42 -0800

    adding foundations for authentication using ASP.NET Core Identity

---
+++ WorkoutBackend.Identity/IdentityServices.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WorkoutBackend.Identity;
+
+public static class IdentityServices
+{
+    public static IServiceCollection AddIdentityContext(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<WorkItOutDbContext>(options => options.UseSqlServer(connectionString));
+
+        return services;
+    }
+}
---
+++ WorkoutBackend.Identity/WorkItOutDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WorkoutBackend.Identity;
+
+public class WorkItOutDbContext : IdentityDbContext
+{
+    public WorkItOutDbContext(DbContextOptions<WorkItOutDbContext> options) : base(options)
+    {
+        
+    }
+}
